453d51236fd72db4d08ca850bc57f21023c6454a
clare3 testing <ms.clarewynn@gmail.com>
2019-03-11 19:18:25 -0500

    testing 3 with git bash

--- test.js
+++ test.js
@@ -1,4 +1,7 @@
-// testing git hub using git bash -- worked
+// testing git hub using git bash -- worked?? But says name was disarranged order
 
 
-// trying again using wsl.exe
+// trying again using wsl.exe  -- did git config, able to link correct username ms.clarewynn@gmaile and nclairesays
+
+
+// drying agian with git bash
